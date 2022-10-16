a3359e10234ef4cdd5437f67b3b41c9a5567ddb4
dennisdlr23 <dennisdelarosa250@gmail.com>
2022-10-16 01:01:43 -0600

    Add project files.

---
+++ Ejercicio1_1/Ejercicio1_1/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void btnsumar_Clicked(object sender, EventArgs e)
+        {
+
+            double n1 = Convert.ToDouble(txtn1.Text);
+            double n2 = Convert.ToDouble(txtn2.Text);
+            double resultado = n1 + n2;
+            Navigation.PushModalAsync(new Views.PageTwo(resultado));
+
+        }
+
+        private void btnrestar_Clicked(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtn1.Text);
+            double n2 = Convert.ToDouble(txtn2.Text);
+            double resultado = n1 - n2;
+            Navigation.PushModalAsync(new Views.PageTwo(resultado));
+        }
+
+        private void btnmultiplicar_Clicked(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtn1.Text);
+            double n2 = Convert.ToDouble(txtn2.Text);
+            double resultado = n1 * n2;
+            Navigation.PushModalAsync(new Views.PageTwo(resultado));
+        }
+
+        private void btndividir_Clicked(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txtn1.Text);
+            double n2 = Convert.ToDouble(txtn2.Text);
+            double resultado = n1 / n2;
+            Navigation.PushModalAsync(new Views.PageTwo(resultado));
+        }
+    }
+}
---
+++ Ejercicio1_1/Ejercicio1_1/Views/PageTwo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio1_1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageTwo : ContentPage
+    {
+        public PageTwo()
+        {
+            InitializeComponent();
+        }
+        public PageTwo(Double resultado)
+        {
+            InitializeComponent();
+
+            lblresultado.Text = "El resultado de la operación es: " + resultado;
+        }
+    }
+}
